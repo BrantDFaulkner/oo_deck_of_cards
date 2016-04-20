346f401cac64aa2e65bf2fd24817b98f65162aa2
Brant Faulkner <brantdfaulkner@gmail.com>
2016-04-19 19:27:27 -0500

    add solutions

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,29 @@
-#write some code here
+
+class Card
+  attr_reader :suit, :rank
+
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
+
+  def face_card?
+    rank > 10
+  end
+
+  def to_s
+    "#{rank_to_name} of #{suit.capitalize}"
+  end
+
+  include Comparable
+  def <=>(other)
+    rank <=> other.rank
+  end
+
+private
+
+  def rank_to_name
+    ["Ace", "2","3","4","5","6","7","8","9","10","Jack","Queen","King"][rank-1]
+  end
+
+end
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,1 +1,57 @@
-#write some code here
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = create_cards
+  end
+
+  def count
+    cards.length
+  end
+
+  def draw(n)
+    cards.pop(n)
+  end
+
+  def shuffle
+    cards.shuffle!
+  end
+
+#shuffle manually
+  # def shuffle
+  #   shuffled = []
+  #   until cards.empty?
+  #     random_card = cards.delete_at(rand(cards.count))
+  #     shuffled << random_card
+  #   end
+  #   self.cards = shuffled
+  # end
+
+private
+
+  def create_cards
+    suits.flat_map do |suit|
+      ranks.map { |rank| Card.new(suit, rank) }
+    end
+  end
+
+#implement with each
+  # def create_cards
+  #   array = []
+  #   suits.each do |suit|
+  #     ranks.each do |rank|
+  #       array << Card.new(suit, rank)
+  #     end
+  #   end
+  #   array
+  # end
+
+  def suits
+    [:hearts, :diamonds, :spades, :clubs]
+  end
+
+  def ranks
+    Array(1..13)
+  end
+
+end
